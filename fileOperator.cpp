98883e1d2249e75ea3e50ccb08ee54e602bc6156
unknown <1843127373@qq.com>
2020-04-06 20:10:09 +0800

    文件操作类

---
+++ fileOperator.cpp
@@ -0,0 +1,145 @@
+#include <sys/stat.h>
+#include <fstream>
+#include <vector>
+#include <iostream>
+
+#include "json.h"
+#include "fileOperator.h"
+
+using json = nlohmann::json;
+
+ const string fileOperator::kSegmentFileNum = "SegmentNum";
+ const string fileOperator::kSourceFileName = "SourceFileName";
+ const string fileOperator::kSegmentFiles = "SegmentFiles";
+ const int fileOperator::kBlockSize = 1024 * 1024;  // 1MB
+
+void fileOperator::segment(string file_name, int segment_num, string json_file) {
+
+    // 检查源文件是否存在
+    if (!exist(file_name)) {
+        cout << "file [" << file_name << "] doesn't exist!" << endl;
+        return;
+    }
+
+    // 检查分片数量是否大于0
+    if (segment_num <= 0) {
+        cout << "segment number should be greater than 0!" << endl;
+        return;
+    }
+
+    // 分片文件名
+    vector<string> segment_files;
+    for (int i = 0; i < segment_num; i++) {
+        segment_files.push_back(file_name + to_string(i+1) + ".tmp");
+        cout << "segment_file --- " << segment_files[i] << endl;
+    }
+
+    ifstream src_file_input(file_name);
+    // 输入文件大小
+    size_t src_file_size = file_size(src_file_input);
+    // 分片文件大小
+    size_t segment_size = src_file_size / segment_num;
+
+    // 分片输出文件
+    for (int i = 0; i < segment_num; i++) {
+        ofstream segment_file_output(segment_files[i]);
+        if (i == segment_num-1) {  // 最后一次，要将剩余文件片全部写入
+            size_t left_size = src_file_size % segment_size;
+            copy_file(src_file_input, segment_file_output, segment_size + left_size);
+        } else {
+            copy_file(src_file_input, segment_file_output, segment_size);
+        }
+        segment_file_output.close();
+    }
+
+    src_file_input.close();
+
+    ofstream json_output(json_file);
+    json j;
+    j[kSegmentFileNum] = segment_num;
+    j[kSourceFileName] = file_name;
+    j[kSegmentFiles] = segment_files;
+    json_output << j;
+    json_output.close();
+}
+
+
+void fileOperator::merge(string json_file) {
+    json j;
+
+    if (!exist(json_file)) {
+        cout << "json file [" << json_file << "] doesn't exist!" << endl;
+        return;
+    }
+
+    ifstream json_input(json_file);
+    json_input >> j;
+
+    // 源文件名
+    string src_file = j[kSourceFileName];
+
+    // 检查源文件是否已经存在
+    if (exist(src_file)) {
+        src_file += ".copy";
+    }
+    ofstream result(src_file);
+
+    // 文件分片数量
+    int segment_num = j[kSegmentFileNum];
+    // 分片文件名
+    vector<string> segment_files = j[kSegmentFiles];
+
+    // 检查文件分片是否齐全
+    for (auto it = segment_files.begin(); it != segment_files.end(); ++it) {
+        if (!exist(*it)) {
+            cout << "segment file [" << *it << "] doesn't exist!" << endl;
+            return;
+        }
+    }
+
+    // 合并文件
+    for (auto it = segment_files.begin(); it != segment_files.end(); it++) {
+        cout << "copy file [" << *it << "]" << endl;
+        ifstream seg_input(*it);
+        size_t seg_input_size = file_size(seg_input);
+        copy_file(seg_input, result, seg_input_size);
+        seg_input.close();
+    }
+
+    json_input.close();
+    result.close();
+}
+
+void fileOperator::copy_file(ifstream &input, ofstream &output, size_t input_size) {
+    char* data = new char[kBlockSize];
+
+    for (size_t block = 0; block < input_size / kBlockSize; block++) {
+        read_file_in_block(data, input);
+        write_file_in_block(data, output);
+    }
+
+    size_t left_size = input_size % kBlockSize;
+    if (left_size != 0) {
+        read_file_in_block(data, input, left_size);
+        write_file_in_block(data, output, left_size);
+    }
+
+    delete [] data;
+    data = nullptr;
+}
+
+bool fileOperator::exist(string name) {
+    struct stat buffer;
+    return (stat(name.c_str(), &buffer) == 0);
+}
+
+
+size_t fileOperator::file_size(ifstream &file) {
+    size_t size;
+    file.seekg(0, std::ios::end);
+    size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    return size;
+}
+
---
+++ fileOperator.h
@@ -0,0 +1,31 @@
+#ifndef FILEOPERATOR_H
+#define FILEOPERATOR_H
+#include <string>
+#include <fstream>
+using namespace std;
+
+class fileOperator {
+public:
+    void segment(string file_name, int segment_num, string json_file="config.json");
+    void merge(string json_file="config.json");
+private:
+    bool exist(string name);
+    size_t file_size(ifstream &file);
+    void copy_file(ifstream &input, ofstream &output, size_t input_size);
+
+    inline void read_file_in_block(char* data, ifstream &input, int size=kBlockSize) {
+        input.read(data, size);
+    }
+    inline void write_file_in_block(char* data, ofstream &output, int size=kBlockSize) {
+        output.write(data, size);
+    }
+private:
+    // json key
+    static const string kSegmentFileNum;
+    static const string kSourceFileName;
+    static const string kSegmentFiles;
+
+    static const int kBlockSize;
+
+};
+#endif // FILEOPERATOR_H
